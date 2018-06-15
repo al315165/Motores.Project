e3d462e74c5daf5d8758a7f62d089b35bbf97b36
sergitim <git config --global user.email sergitim@gmail.com>
2018-06-15 08:59:47 +0200

    Collission boxes fixed + Early Start of next wave supposedly fixed
    Collission boxes fixed + Early Start of next wave supposedly fixed

--- Die Nacht vom Terror/Assets/Script/EnemyScript.js
+++ Die Nacht vom Terror/Assets/Script/EnemyScript.js
@@ -16,7 +16,16 @@ if (EnemyHealth <= 0)
 	Destroy(gameObject);
 	Spawn.GetComponent(EnemySpawn).EnemiesLeft.RemoveAt(0);
 	Gun.GetComponent(GunFire).score += 5;
-	if (Spawn.GetComponent(EnemySpawn).EnemiesLeft.Count <= 0){Spawn.GetComponent(EnemySpawn).Spawn();}
+
+	Debug.Log("Repetition: " + Spawn.GetComponent(EnemySpawn).repetition + " Timer: " + Spawn.GetComponent(EnemySpawn).timer);
+
+	if 
+	(
+		Spawn.GetComponent(EnemySpawn).EnemiesLeft.Count <= 0 && Spawn.GetComponent(EnemySpawn).timer == Spawn.GetComponent(EnemySpawn).repetition
+	)
+
+	{Spawn.GetComponent(EnemySpawn).Spawn(); }
+
 	}
 	}
  	 
